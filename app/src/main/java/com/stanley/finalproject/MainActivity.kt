7057545e696e464cd545c0ef8dd8bdfc71ce9537
stanleystn <stanleysetiawan321@gmail.com>
2023-07-28 16:46:06 +0700

    feat: add disaster item layout

--- app/src/main/java/com/stanley/finalproject/MainActivity.kt
+++ app/src/main/java/com/stanley/finalproject/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.stanley.finalproject
 
 import android.os.Bundle
+import android.widget.SearchView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,9 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.stanley.finalproject.ui.theme.FinalProjectTheme
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var searchView: SearchView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,10 +28,19 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    recyclerView = findViewById(R.id.recyclerView)
+                    searchView = findViewById(R.id.searchView)
+
+                    recyclerView.setHasFixedSize(true)
+                    recyclerView.layoutManager = LinearLayoutManager(this)
+
+                    addDataToList()
                 }
             }
         }
+    }
+    private fun addDataToList(){
+
     }
 }
 
